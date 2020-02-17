79da1d2e997449640c372aab3ca1d9170cfc8971
Sean Corcoran <sdc76@cornell.edu>
2020-02-17 16:48:46 -0500

    Add routes for getting deleting and updating a user

--- src/repos/UserRepo.ts
+++ src/repos/UserRepo.ts
@@ -9,32 +9,88 @@ const createUser = async (
   firstName: string,
   lastName: string,
 ): Promise<User> => {
-  try {
-    if (!(netID && googleID && firstName && lastName)) {
-      throw Error('Invalid parameters');
-    }
-    const user = db().create({
-      netID,
-      googleID,
-      firstName,
-      lastName,
-    });
-    await db().save(user);
-    return user;
-  } catch (e) {
-    throw Error('Unable to create user');
+  if (!(netID && googleID && firstName && lastName)) {
+    throw Error('Invalid parameters');
   }
+  const possible_user = await db().findOne({ netID });
+  if (!(possible_user == null)) {
+    throw Error('User with that netID already exists')
+  }
+  const user = db().create({
+    netID,
+    googleID,
+    firstName,
+    lastName,
+  });
+  await db().save(user);
+  return user;
+};
+
+const deleteUser = async (
+  netID: string
+): Promise<Boolean> => {
+  if (!(netID)) {
+    throw Error('Invalid parameter');
+  }
+  await db()
+    .createQueryBuilder()
+    .delete()
+    .from(User)
+    .where("netID = :netID", { netID: netID })
+    .execute()
+    .then(dr => {
+      if (dr.affected == 0) {
+        throw Error('User does not exist')
+      }
+    }
+    )
+    .catch(e => {
+      throw Error('Unable to execute sql')
+    }
+    )
+  return true
+};
+
+const updateUser = async (
+  currentNetID: string,
+  firstName: string,
+  netID: string,
+  lastName: string
+): Promise<Boolean> => {
+  await db()
+    .createQueryBuilder()
+    .update(User)
+    .set({
+      firstName: firstName,
+      netID: netID,
+      lastName: lastName
+    })
+    .where("netID = :netID", { netID: currentNetID })
+    .execute()
+    .then(ur => {
+      if (ur.affected == 0) {
+        throw Error('User does not exist')
+      }
+    }
+    )
+    .catch(e => {
+      throw Error('Unable to execute sql')
+    }
+    )
+  return true
 };
 
 const getUserByNetID = async (netID: string): Promise<User> => {
-  try {
-    return db().findOne({ netID });
-  } catch (e) {
-    throw Error('Unable to find user');
+  const user = await db().findOne({ netID });
+  if (user == null) {
+    throw Error('User with that netID does not exist')
   }
+  return user
 };
 
 export default {
   createUser,
+  deleteUser,
   getUserByNetID,
+  updateUser
 };
