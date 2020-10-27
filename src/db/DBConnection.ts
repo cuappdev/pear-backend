491d4a57df878f2e846a021f056c47335158a148
Gonzalo Gonzalez <gg387@cornell.edu>
2020-10-27 19:47:49 -0400

    Small fix

--- src/db/DBConnection.ts
+++ src/db/DBConnection.ts
@@ -2,13 +2,13 @@ import { Connection, ConnectionOptions, createConnection } from 'typeorm';
 import Club from '../entities/Club';
 import CornellMajor from '../entities/CornellMajor';
 import Interest from '../entities/Interest';
-import Time from '../entities/Availability';
+import Availability from '../entities/Availability';
 import User from '../entities/User';
 import UserSession from '../entities/UserSession';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const models = [Club, CornellMajor, Interest, Time, User, UserSession];
+const models = [Club, CornellMajor, Interest, Availability, User, UserSession];
 
 const connectionOptions: ConnectionOptions = {
   database: process.env.DB_NAME,
